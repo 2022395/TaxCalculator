e5f80e4d9a5c21374b61045e63f1aa5e6a76f22f
2022395 <2022395@student.cct.ie>
2023-12-18 16:05:35 +0000

    Created main class
    added uscCalculator method

--- src/taxcalculator/TaxCalculator.java
+++ src/taxcalculator/TaxCalculator.java
@@ -5,8 +5,9 @@
 package taxcalculator;
 
 /**
- *
- * @author volka
+ *https://github.com/2022395/TaxCalculator
+ * @author Volkan Tibet Tuylu-2022395-
+ * @author Ana Clara Quilapan-2022475-
  */
 public class TaxCalculator {
 
@@ -14,7 +15,34 @@ public class TaxCalculator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
+    //Calculates the usc
+        public static Double uscCalculator(double income) {
+        
+ // €0 – €12,012 @ 0.5%  €12,013 – €22,920 @ 2% €22,921 – €70,044 @ 4.5%
+        double uscRate = 0;
+         if (income <= 12012) {
+            uscRate = 0.005; // 0.5%
+        } else if (income <= 22920) {
+            uscRate = 0.02;  // 2%
+        } else if (income <= 70044) {
+            uscRate = 0.045; // 4.5%
+        } else if (income>=70044.01){
+            uscRate = 0.08;   // %8 for income above €70,044
+        } 
+        else{
+            System.out.println("Make sure you've put correct income and try again!");
+        
+        }
+        //convert to the weekly income
+        double usc = income/52 * uscRate;
+
+        System.out.println("Universal Social Charge is: " + usc +" Euro");
+      
+         return usc;
+        
+
+}
     
 }
