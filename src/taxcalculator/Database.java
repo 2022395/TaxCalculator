c91c7a460779adb5fb60cb1af5fd1dcc3b3de06e
2022475 <2022475@student.cct.ie>
2023-12-18 17:34:12 +0000

    Update Database.java

--- src/taxcalculator/Database.java
+++ src/taxcalculator/Database.java
@@ -8,6 +8,18 @@ package taxcalculator;
  *
  * @author Clara
  */
-public class Database {
+public abstract class Database {
+    
+     protected final static String DB_BASE_URL= "jdbc:mysql://localhost";
+    protected final static String USER = "ooc2023";
+    protected final static String PASSWORD = "ooc2023";
+    protected final static String DB_NAME= "tax";
+    protected final static String TABLE_NAME= "taxPayerData";
+    
+    
+    
+    protected final static String DB_URL = DB_BASE_URL + "/" + DB_NAME;
+    
+    
     
 }
